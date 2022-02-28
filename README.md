70d80a0043a45a42a85a3ea90ada62555d3c04b1
Chris Chase <cchase@redhat.com>
2022-02-28 10:58:59 -0500

    Add device to s3 path

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 2022-ai-sensor
 
 
+```shell
+curl -X POST -H "Content-Type: application/json" --data '@test-data/test-data.json' http://localhost:5000/predictions
+```
--- service/utilities/s3.py
+++ service/utilities/s3.py
@@ -2,6 +2,7 @@ import os
 import boto3
 import botocore
 import datetime
+import json
 
 s3 = None
 
@@ -10,7 +11,7 @@ s3 = None
 key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 bucket = os.environ.get('AWS_S3_BUCKET')
-prefix = os.environ.get('AWS_S3_PREFIX', 'ai-sensor')
+prefix = os.environ.get('AWS_S3_PREFIX', 'ai-sensor-v3')
 
 if key_id and secret_key and bucket:
     session = boto3.session.Session(aws_access_key_id=key_id,
@@ -19,17 +20,17 @@ if key_id and secret_key and bucket:
         's3',
         config=botocore.client.Config(signature_version='s3v4')
     )
-    print(f'S3 information found.  Will save request body to bucket {bucket}/{prefix}')
+    print(f'S3 information found.  Will save request body to bucket {bucket}/{prefix}/device')
 else:
     print(f'S3 information not found.  Will NOT save request body')
-
 
 
 def save_json(data):
     if s3 is None:
         return
-
+    body = json.loads(data)
+    device = body.get('device') or 'unknown-device'
     obj_key = f'{datetime.datetime.now().isoformat()}.json'
-    s3.Object(bucket, os.path.join(prefix, obj_key)).put(Body=data)
+    s3.Object(bucket, os.path.join(prefix, device, obj_key)).put(Body=data)
 
 
